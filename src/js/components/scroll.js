8e8c1c06d7433b0541360d0816e5bccacdff39b9
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-08-19 18:33:02 +0300

    fix

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -15,6 +15,19 @@ $(document).on('click', 'nav a', '.footer__item a', function (e) {
   //   trigger2.removeClass('is-open');
 });
 
+$(document).on('click', '.footer__item a', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+
+  var top = $(id).offset().top; // получаем координаты блока, узнаем высоту от начала страницы до блока на который ссылается якорь
+  $('body, html').animate({ scrollTop: top - 65 }, 1000); // плавно переходим к блоку
+
+  //   trigger1 = $('.menu-container');
+  //   trigger1.removeClass('is-open');
+  //   trigger2 = $('.hamburger');
+  //   trigger2.removeClass('is-open');
+});
+
 // // на чистому JS
 // const anchors = document.querySelectorAll('a[href*="#"]');
 // anchors.forEach(anchor => {
