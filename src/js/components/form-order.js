7b9f812ea36d64ba76a2e31241e100463b55110e
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2023-05-08 18:11:48 +0300

    adds validator for form

--- src/js/components/form-order.js
+++ src/js/components/form-order.js
@@ -1,18 +1,43 @@
 import getRefs from '../refs';
+import { validateForm } from './validator-form-order';
 import { clearBasket } from './basket';
 import { toast } from './toast';
 import { closeModal } from './modal-basket';
 
 const refs = getRefs();
 
+/* Прослуховувач і обробник на подію відправки форми через натискання кнопки з типом submit у формі */
 refs.btnFormSubmit.addEventListener('click', onFormSubmit);
 
 function onFormSubmit(e) {
-  const form = refs.formBasket;
   e.preventDefault();
-  toast("Дякуємо за замовлення. Менеджер зв'яжеться з Вами найближчим часом.");
-  form.reset();
-  closeModal();
-  clearBasket();
-  //   refs.formOrder.style.display = 'none';
+  if (validateForm() === false) {
+    return;
+  } else {
+    const form = refs.formBasket;
+    toast(
+      "Дякуємо за замовлення. Менеджер зв'яжеться з Вами найближчим часом."
+    );
+    form.reset();
+    closeModal();
+    clearBasket();
+  }
 }
+
+// /* Введення в поле номеру телефона з включеним початковим значенням */
+// refs.formBasket
+//   .querySelector('input[name="tel"]')
+//   .addEventListener('input', e => {
+//     const input = e.target;
+//     if (input.value.length < 4) {
+//       input.value = '+38';
+//       input.setSelectionRange(input.value.length, input.value.length);
+//     }
+//   });
+
+// /* Заборона видаляти початкове значення номеру телефона при фокусі в полі введення */
+// refs.formBasket
+//   .querySelector('input[name="tel"]')
+//   .addEventListener('focus', e => {
+//     e.target.removeAttribute('readonly');
+//   });
---
+++ src/js/components/validator-form-order.js
@@ -0,0 +1,98 @@
+import validator from 'validator';
+import getRefs from '../refs';
+import { toast } from './toast';
+
+const refs = getRefs();
+
+export function validateForm() {
+  let userName = refs.formBasket.querySelector('input[name="user-name"]');
+  let userPhone = refs.formBasket.querySelector('input[name="tel"]');
+  let userEmail = refs.formBasket.querySelector('input[name="mail"]');
+  let userAgree = refs.formBasket.querySelector('input[name="user-agreement"]');
+
+  const errorMessageFormat = name => {
+    toast(
+      `Введіть значення поля "${name.previousElementSibling.textContent}" у вірному форматі`
+    );
+  };
+
+  const errorMessage = name => {
+    toast(
+      `Поле "${name.previousElementSibling.textContent}" обов'язкове для заповнення`
+    );
+  };
+
+  if (
+    validator.isEmpty(userEmail.value) &&
+    validator.isEmpty(userPhone.value) &&
+    validator.isEmpty(userName.value) &&
+    !userAgree.checked
+  ) {
+    toast(
+      `Поля "${userName.previousElementSibling.textContent}", "${userPhone.previousElementSibling.textContent}", "${userEmail.previousElementSibling.textContent}" обов'язкові для заповнення`
+    );
+    return false;
+  }
+
+  if (validator.isEmpty(userName.value)) {
+    errorMessage(userName);
+    return false;
+  }
+
+  if (!validator.isLength(userName.value, { min: 3, max: 12 })) {
+    toast(
+      `В поле "${userName.previousElementSibling.textContent}" введіть кількість символів від 3 до 12`
+    );
+    return false;
+  }
+
+  if (validator.isEmpty(userPhone.value)) {
+    errorMessage(userPhone);
+    return false;
+  }
+
+  if (
+    !validator.matches(
+      userPhone.value,
+      /^(\+38|38|8)?[\s-.]?\d{3}[\s-.]?\d{2}[\s-.]?\d{2}[\s-.]?\d{3}$/
+    )
+  ) {
+    errorMessageFormat(userPhone);
+    return false;
+  }
+
+  if (validator.isEmpty(userEmail.value)) {
+    errorMessage(userEmail);
+    return false;
+  }
+
+  if (!validator.isEmail(userEmail.value)) {
+    errorMessageFormat(userEmail);
+    return false;
+  }
+
+  if (!userAgree.checked) {
+    toast(
+      `Прийняття умов договору є обов'язковим для підтвердження замовлення`
+    );
+    return false;
+  }
+}
+
+/* Введення в поле номеру телефона з включеним початковим значенням */
+refs.formBasket
+  .querySelector('input[name="tel"]')
+  .addEventListener('input', e => {
+    const input = e.target;
+    if (input.value.length < 4) {
+      input.value = '+38';
+      input.setSelectionRange(input.value.length, input.value.length);
+    }
+  });
+
+/* Заборона видаляти початкове значення номеру телефона при фокусі в полі введення */
+refs.formBasket
+  .querySelector('input[name="tel"]')
+  .addEventListener('focus', e => {
+    e.target.removeAttribute('readonly');
+  });
