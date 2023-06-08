1c2063c282f7aca0d720ca8a239d6fea27f1d706
Maryna Skrypnyk <74303774+Maryna-Skrypnyk@users.noreply.github.com>
2023-06-08 14:10:59 +0300

    up

--- src/js/scale-input.js
+++ src/js/scale-input.js
@@ -5,15 +5,15 @@ export function scaleInput() {
     ).some(el => {
       return el === e.target || el.contains(e.target);
     });
-    if (isInputFocused) {
-      e.preventDefault();
-    }
-    // if (!isInputFocused) {
-    //   const focused = document.querySelector('input:focus');
-    //   if (focused) {
-    //     focused.blur();
-    //   }
+    // if (isInputFocused) {
+    //   e.preventDefault();
     // }
+    if (!isInputFocused) {
+      const focused = document.querySelector('input:focus, textarea:focus');
+      if (focused) {
+        focused.blur();
+      }
+    }
   });
 }
 
